086f5c2e8a0522c09e4cbb27a74e8382357b90ce
Petr <zpk2006@mail.ru>
2013-12-21 17:58:43 +0400

    I2C complete
    Дописана библиотека I2C. Проверка:
    запрос идентификатора акселерометра и вывод его на светодиоды.
    Успешно: полученный двоичный код совпадает с указанным в документации.

--- main.c
+++ main.c
@@ -4,29 +4,28 @@ eeprom int x=0;
 
 void main(void)
 {
-    char lol;
+    char lol;  
+    char y_adress=0x0F;
     acc_init(); 
     //DDRD=0x00;
     DDRB=0xff;
     //PORTD=0x00;
     PORTB=0x01;
     while(1)
-    {    
-        if(PORTB==0x80)
-        {
-            PORTB=0x01;
-        }              
-        else
-        {   
-            send_start();
-            send_byte(adress<<1);
-            lol=get_sak();    
-            send_finish();
-            PORTB |=(lol<<7);
-            if(!lol)
-                PORTB=PORTB<<1;
-        }   
-        delay_ms(1000);    
+    {                   
+        send_start();
+        send_byte(adress<<1);
+        lol=get_sak();
+        send_byte(y_adress); 
+        lol=get_sak();
+        repeat_start();
+        send_byte((adress<<1)+1);   
+        lol=get_sak();
+        PORTB=get_byte();
+        send_mak(0);   
+        send_stop();   
+        
+        delay_ms(10);    
     } 
     x=0;
 }
