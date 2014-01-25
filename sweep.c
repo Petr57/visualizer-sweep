fb16ecc04203be1ccfd258211f0307be07b57ed0
Petr <zpk2006@mail.ru>
2014-01-25 19:25:47 +0400

    algorithm failure
    по необъяснимой причине развёртка работает только в одном направлении.
    кроме того есть серьёзная проблема в подсчёте текущей линии,
    заключающаяся в погрешности при делении целых чисел. вариантом решения
    может стать явный анализ части (через несколько вложенных констрункций
    if else).

--- sweep.c
+++ sweep.c
@@ -1,9 +1,9 @@
 #include <sweep.h> 
  
-__flash char Frame[]={0xf0,0x88,0x8c,0x88,0xf0,0x88,0x88,0xf0}; //symbol B //{0x01,0x02,0x04,0x08,0x10,0x20,0x40,0x80};//{0x86,0x86,0xfe,0x86,0x86,0x86,0x7e,0x00}; //symbol A
-
+char Frame[]={0x01,0x02,0x04,0x08,0x10,0x20,0x40,0x80};//{0xf0,0x88,0x8c,0x88,0xf0,0x88,0x88,0xf0}; //symbol B //{0x86,0x86,0xfe,0x86,0x86,0x86,0x7e,0x00}; //symbol A
+    
 signed char last_min=0,last_max=0,last_value=0;
-char direction,line_pointer,dir_counter=2;  //0,1 - negative direction dir=1; 2,3 - positive direction dir=0;
+char direction=0,line_pointer,dir_counter=2;  //0 - negative direction=1; 2 - positive direction=0;
 
 void sweep_init()
 {      
@@ -12,11 +12,16 @@ void sweep_init()
 }
 
 void set_line()
-{  
+{    
     if(direction)
-        FRAMEPORT=Frame[7-line_pointer];          //set output line
-    else        
-        FRAMEPORT=Frame[line_pointer];  
+    {
+        FRAMEPORT=0x00;//Frame[7-line_pointer];          //set output line 
+    }
+    else 
+    {       
+        FRAMEPORT=Frame[line_pointer];
+    }
+    if(line_pointer<7) line_pointer++; 
 }
 
 void sweep_update(void)
@@ -25,35 +30,38 @@ void sweep_update(void)
     value=get_Y();
     if(value<last_min) last_min=value;
     if(value>last_max) last_max=value;
-    if((value>last_value)&&(dir_counter<3))     dir_counter++;  //value rising -> dir_counter++
+    if((value>last_value)&&(dir_counter<2))     dir_counter++;  //value rising -> dir_counter++
     if((value<last_value)&&(dir_counter>0))     dir_counter--;  //value falling -> dir_counter--  
     
     line_width=(last_max-last_min)/8;
     if(direction)
-    { 
+    {  
+        if(value<last_max-line_width*(line_pointer))
+        {
+            set_line();
+        }
         if(dir_counter>1)
         {
             direction=0;
-            last_min=value;
+            last_min=last_value;
             line_pointer=0;
-        }
-        if(value<last_max-line_width*(line_pointer+1))
-        {
             set_line();
-        }
+        } 
     }
     else
-    {
-        if(dir_counter<2)
-        {
-            direction=1;
-            last_max=value;
-            line_pointer=0;
-        }
-        if(value>last_min+line_width*(line_pointer+1))
+    {    
+        if(value>(last_min+line_width*line_pointer))
         {
             set_line();
-        }
-    }
+        } 
+        if(dir_counter<1)
+        {
+            direction=1;
+            last_max=last_value;
+            line_pointer=0;
+            set_line();
+        } 
+    }    
+    last_value=value;
 }
 
